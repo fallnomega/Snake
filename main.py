5e9dfe702e8fe33555b40e7ec033cc5eb1affe0a
fallnomega <fallnomega@gmail.com>
2023-02-07 09:29:37 -0800

    working on moving the snake

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ import turtle as t
 #TODO - detect collision with tail
 
 
-
+def move_foward():
+    for x in snake:
+        x.forward(20)
+def turn_right():
+    for x in snake:
+        x.setheading(270)
+def turn_left():
+    for x in snake:
+        x.setheading(90)
 
 
 screen = t.Screen()
@@ -18,19 +26,32 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 
 #make snake
-def make_snake():
-    x_pos = -30
-    snake.shapesize(1,-3,1)
-    snake.color("white")
-    snake.shape("square")
-    snake.goto(-30,0)
+def make_snake(i,x_pos):
+    # snake.shapesize(1,-3,1)
+    x.penup()
+    i.color("white")
+    i.shape("square")
+    i.goto(x_pos,0)
 
 game_is_on = True
-snake = t.Turtle()
-snake.penup()
+snake = []
+x_position = 0
+for x in range(3):
+    x = t.Turtle()
+    make_snake(x,x_position)
+    snake.append(x)
+    x_position-=20
 
-make_snake()
-while game_is_on:
-    snake.forward(20)
+screen.onkeypress(move_foward, 'w')
+screen.onkeypress(turn_left, 'a')
+screen.onkeypress(turn_right, 'd')
+
+screen.listen()
+
+# while game_is_on:
+# #     # snake.forward(20)
+#     screen.onkeypress(move_foward, 'w')
+#     screen.listen()
+
 
 screen.exitonclick()
