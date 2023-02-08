d1043a97707554483d1c6331a5fa82a55c5b0a75
fallnomega <fallnomega@gmail.com>
2023-02-08 10:02:01 -0800

    code refactor

--- main.py
+++ main.py
@@ -47,11 +47,10 @@ while game_is_on:
     # detect collision with tail
     # if player.head collides with any segment of the tail, trigger GAMEOVER
 
-    for segment in player.segments[1:len(player.segments)-1]:
-        if segment == player.head:
-            pass
-        elif player.head.distance(segment) < 10:
-            scoreboard.game_over()
+    for segment in player.segments[1:]:
+        if player.head.distance(segment) < 10:
             game_is_on = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
